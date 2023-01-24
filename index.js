d0bd0be805a1013fba9831d3fbb1f7d9848fe970
alberto-mm <alberto.martin.montero@gmail.com>
2023-01-24 18:57:26 +0100

    Fin sección 10 - Auth

--- index.js
+++ index.js
@@ -11,21 +11,17 @@ const app = express();
 // Configurar CORS
 app.use( cors() );
 
+// Lectura y parseo del body
+app.use( express.json() );
+
 // Base de datos
 dbConnection();
-
 // DB_USER: mean_user
 // DB_PASSWORD: vqvHww35vjBKYhJ8
 
 // Rutas
-app.get('/', (req, res) => {
-
-    res.status(400).json({
-        ok: true,
-        msg: 'Hola mundo'
-    });
-
-});
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto ' + process.env.PORT);
